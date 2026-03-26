01e643e93c0287a368a8e6f446d94dabbda99188
DanniShen0329 <shendn@foxmail.com>
2026-03-26 12:47:08 +0800

    修改测试环境配置

--- tests/config/test.py
+++ tests/config/test.py
@@ -5,6 +5,6 @@ from .settings import BaseConfig
 class TestConfig(BaseConfig):
     """测试环境配置"""
     env: str = "test"
-    base_url: str = "http://172.17.12.107:9909"
-    db_host: str = "172.17.12.217:1521"
-    db_name: str = "cdlddb"
+    base_url: str = "http://xxxxx:9909"
+    db_host: str = "xxxx:1521"
+    db_name: str = "db"
